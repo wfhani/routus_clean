ecbd6ef78570f88665c867edc462b311ad14cff2
shahdmahmmoud <shahdmahmoud172@gmail.com>
2025-03-08 23:18:51 +0200

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:shared_project/features/parent_home/presentation/screens/home.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'core/theme/app_theme.dart';
 import 'features/auth/presentation/screens/forgot_password.dart';
 import 'features/auth/presentation/screens/sign_in_screen.dart';
@@ -8,16 +9,21 @@ import 'features/auth/presentation/screens/sign_up_screen.dart';
 import 'features/onboarding/onboarding1/onboarding1.dart';
 import 'features/onboarding/onboarding2/onboarding2.dart';
 import 'features/onboarding/onboarding3/onboarding3.dart';
-import 'features/onboarding/spla.dart';
-import 'features/onboarding/splash.dart';
-import 'features/role_selection/presentation/screens/role_selection_screen.dart';
 import 'features/settings/presentation/screens/help&support/contact_us.dart';
-import 'features/settings/presentation/screens/settings/settings_screen.dart';
 
 
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-void main() {
-  //init();
+  try {
+    await Firebase.initializeApp();
+    FirebaseMessaging.instance.getToken().then((token) {
+      print("🔥 FCM Token: $token");
+    });
+  } catch (e) {
+    print("❌ Firebase initialization error: $e");
+  }
+
   runApp(MyApp());
 }
 
@@ -31,10 +37,10 @@ class MyApp extends StatelessWidget {
       builder: (_, child) {
         return MaterialApp(
           debugShowCheckedModeBanner: false,
-          theme: appTheme, // Use the theme defined in app_theme.dart
+          theme: appTheme, // Using the custom theme
           initialRoute: '/',
           routes: {
-            '/': (context) => HomeScreen (),
+            '/': (context) => SignInScreen(),
             '/signin': (context) => SignInScreen(),
             '/signup': (context) => SignUpScreen(),
             '/forgetpassword': (context) => ForgetPasswordScreen(),
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,20 @@
 import FlutterMacOS
 import Foundation
 
+import facebook_auth_desktop
+import firebase_core
+import firebase_messaging
+import flutter_local_notifications
+import flutter_secure_storage_macos
+import google_sign_in_ios
+import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FacebookAuthDesktopPlugin.register(with: registry.registrar(forPlugin: "FacebookAuthDesktopPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
